26c007dff6832ff73bb0b94b5fec7ecc8dbc75ac
Thenmozhy <thenmozhy.d@reancloud.com>
2018-01-13 22:20:15 +0530

    Update opis-ftp-files.sh

--- CMPcases/opis-ftp-files.sh
+++ CMPcases/opis-ftp-files.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 source_path="/opis/ftp/users/opis"
-backup_path="/opis/ftp/temp"
+log_path="/opis/ftp/temp"
+backup_path="/bk-opis/backup"
 cd $source_path
-ftp_31_days_old_files=$(find -type f -mtime +31 | sed 's/.//')
 today_date=$(date "+%d-%m-%Y")
-echo "$ftp_31_days_old_files" >> "$backup_path/"ftp_31_days_old_files_$today_date
+find -type f -mtime +31 | sed 's/.//' > "$log_path/"ftp_31_days_old_files_$today_date
 file_name=$(ls "$backup_path/"ftp_31_days_old_files_$today_date)
 file_list=$(cat $file_name)
+
 for files in $file_list
 do
 cp -v --parents -p $source_path$files $backup_path >> "$backup_path/"files_moved_logs_$today_date 
-done
-for files in $file_list
-do
 rm -vf $source_path$files >> "$backup_path/"files_moved_logs_$today_date
 done
